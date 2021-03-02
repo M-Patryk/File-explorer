977fda2745c269a0029fd2e99858fc9d1f0a9059
Patryk <47945758+M-Patryk@users.noreply.github.com>
2021-03-02 16:02:49 +0100

    Option one of checking if item is file or folder

---
+++ difWayCheckingItems.js
@@ -0,0 +1,36 @@
+//Option #1
+// fs.readdir(process.cwd(), (err, filenames) => {
+//     //Either err === error object, which means something went wrong
+//     // OR err === 0, which means everything is OK
+//     if(err){
+//         // console.log(err) //If i do this and error occur but code wont crash then if it is possible the code will still be running
+//         throw new Error(err) // w tym przypadku jesli wystapi problem to od razu program sie zatrzyma
+//     }
+
+//     const allStats = Array(filenames.length).fill(null)
+    
+//     for(let filename of filenames){
+//         const index = filenames.indexOf(filename)
+
+//         fs.lstat(filename, (err, stats) => {
+//             if(err){
+//                 console.log(err)
+//             }
+
+//             allStats[index] = stats
+
+//             const ready = allStats.every(stats => {
+//                 return stats
+//             })
+
+//             if(ready){
+//                 allStats.forEach((stats, index) =>{
+//                     console.log(filenames[index], stats.isFile())
+//                 })
+//             }
+//         })
+//     }
+// })
+
+
+//Option #2

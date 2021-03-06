b5f25329a1b54a3a4d9b6e747af695f088db0a9e
Patryk <47945758+M-Patryk@users.noreply.github.com>
2021-03-06 09:52:58 +0100

    Changed the way of checking for file info and Added colors whether the item is file or folder

--- index.js
+++ index.js
@@ -1,39 +1,34 @@
 #!/usr/bin/env node
 const fs = require('fs');
 const util = require('util');
+const chalk = require('chalk');
+const path = require('path');
 
-fs.readdir(process.cwd(), async (err, filenames) => {
+//Method #2
+const lstat = util.promisify(fs.lstat);
+
+const targetDirectory = process.argv[2] || process.cwd();
+fs.readdir(targetDirectory, async (err, filenames) => {
 	//Either err === error object, which means something went wrong
 	// OR err === 0, which means everything is OK
 	if (err) {
 		// console.log(err) //If i do this and error occur but code wont crash then if it is possible the code will still be running
 		throw new Error(err); // w tym przypadku jesli wystapi problem to od razu program sie zatrzyma
 	}
-	for (let filename of filenames) {
-		try {
-			const stats = await lstat(filename);
-			console.log(filename, stats.isFile());
-		} catch (err) {
-			console.log(err);
+	const statPromieses = filenames.map((filename) => {
+		return lstat(path.join(targetDirectory, filename)); //Tutaj tworzymy tablice z obiektami plikow ze zmiennej filenames. Obiekty maja info o danym pliku/folderze
+	});
+
+	const allStats = await Promise.all(statPromieses); //To do gory to jest tylko obietnica, ale jeszcze nie spelniona. Tutaj ja spelniamy
+
+	for (let stats of allStats) {
+		const index = allStats.indexOf(stats); // Rozbijamy obiekty z tablicy na czesci i wyswietlamy nazwe pliku wraz z funkcja isFIle()
+
+		// console.log(filenames[index], stats.isFile())
+		if (stats.isFile()) {
+			console.log(chalk.green(filenames[index]));
+		} else {
+			console.log(chalk.yellow(filenames[index]));
 		}
 	}
 });
-
-//Method #1 - has to be at the bottom
-const lstat = (filename) => {
-	return new Promise((resolve, reject) => {
-		fs.lstat(filename, (err, stats) => {
-			if (err) {
-				reject(err);
-			}
-
-			resolve(stats);
-		});
-	});
-};
-
-//Method #2
-// const lstat = util.promisify(fs.lstat)
-
-//Method #3
-//const {lstat} = fs.promises;
